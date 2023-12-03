999dd70d51ce171dd33019968d4bd2586d76c85c
iHux <hugo.pristauz@gmail.com>
2023-12-03 15:14:13 +0000

    setup various tokens in Toy

--- neurotron/src/neurotron/cluster/toy.py
+++ neurotron/src/neurotron/cluster/toy.py
@@ -3,20 +3,49 @@ module neurotron.cluster.toy
     class Toy  # to provide toy stuff for neurotron cluster
 """
 
+from neurotron.cluster.cluster import Token
+
 #===============================================================================
 # class Toy
 #===============================================================================
 
 class Toy:
     """
+    >>> Toy()
+    Toy('Sarah')
     >>> Toy('Mary')
     Toy('Mary')
     """
-    def __init__(self,tag):
+    def __init__(self,tag='Sarah'):
         self.tag = tag
+        if tag == 'Sarah': self._sarah()
+        if tag == 'Mary':  self._mary()
 
     def __str__(self):
         return "Toy('%s')" % self.tag
+
+    def _sarah(self):
+       self.token = Token({
+           'Sarah':[1,1,0,1,1,1,0,1,0,1],
+           'loves':[0,1,1,1,0,1,1,0,1,1],
+           'music':[1,1,1,0,0,1,0,1,1,1],
+           '.':    [0,0,0,0,0,0,0,0,0,0],
+           })
+
+    def _mary(self):
+        self.token = Token({
+            'Mary': [1,0,0,0,0,0,0,1,1],
+            'John': [0,1,0,0,0,0,0,1,1],
+            'Lisa': [1,0,0,0,0,0,1,1,0],
+            'Andy': [0,1,0,0,0,0,1,1,0],
+            'likes':[0,0,1,0,0,0,0,1,1],
+            'to':   [0,0,0,1,0,0,0,1,1],
+            'sing': [0,0,0,0,1,0,0,1,1],
+            'dance':[0,0,0,0,0,1,0,1,1],
+            'hike': [0,0,0,0,1,0,1,1,0],
+            'climb':[0,0,0,0,1,0,1,0,1],
+            '.':    [0,0,0,0,0,0,1,1,1],
+            })
 
     def __repr__(self):
         return self.__str__()
